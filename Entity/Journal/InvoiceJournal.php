521c3faea4aeda7723b49799890faf4c688c0ec2
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2013-02-23 15:41:40 -0500

    Added relationshio from InvoiceJournal to Invoice

--- Entity/Journal/InvoiceJournal.php
+++ Entity/Journal/InvoiceJournal.php
@@ -21,4 +21,54 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class InvoiceJournal extends Journal
 {
+    /**
+     * @ORM\ManyToOne(targetEntity="\HarvestCloud\InvoiceBundle\Entity\Invoice", inversedBy="journals")
+     * @ORM\JoinColumn(name="invoice_id", referencedColumnName="id")
+     */
+    protected $invoice;
+
+    /**
+     * __construct()
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2013-02-23
+     *
+     * @param  HarvestCloud\InvoiceBundle\Entity\Invoice
+     */
+    public function __construct(\HarvestCloud\InvoiceBundle\Entity\Invoice $invoice)
+    {
+        parent::__construct();
+
+        $this->setInvoice($invoice);
+    }
+
+    /**
+     * Set invoice
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2013-02-23
+     *
+     * @param  \HarvestCloud\InvoiceBundle\Entity\Invoice $invoice
+     *
+     * @return InvoiceJournal
+     */
+    public function setInvoice(\HarvestCloud\InvoiceBundle\Entity\Invoice $invoice = null)
+    {
+        $this->invoice = $invoice;
+
+        return $this;
+    }
+
+    /**
+     * Get invoice
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2013-02-23
+     *
+     * @return \HarvestCloud\InvoiceBundle\Entity\Invoice
+     */
+    public function getInvoice()
+    {
+        return $this->invoice;
+    }
 }
